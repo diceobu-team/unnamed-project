ca97a66c3714b737a01371bbb387c0222935bf3e
obdsf <Geo.Giotopoulos@gmail.com>
2019-08-25 19:58:25 +0300

    new loader, tweaks in navbar, other fixes

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// CSS Loader
+function onPageLoad() {
+  $(window).on("load", function() {
+    setTimeout(function() {
+      var preloader=$('.sk-folding-cube-wrapper');
+      preloader.fadeOut(1000);  // Loader Fadeout Time
+    }, 2000); // Fake Loading Time (Default: 2000, Dev Mode: 0)
+  });
+}
+
+// Language Selector
+function changeLang() {
+  console.log("changeLang\tStarted");
+  if(lang=="Eng") {
+    console.log("changeLang\telemGr");
+    Array.prototype.forEach.call(elemEng, changeToNone);
+    Array.prototype.forEach.call(elemGr, changeToInitial);
+    document.getElementById("dt-change-lang-button").innerHTML="Gr";
+    lang="Gr";
+  } else if(lang=="Gr") {
+    console.log("changeLang\telemEng");
+    Array.prototype.forEach.call(elemGr, changeToNone);
+    Array.prototype.forEach.call(elemEng, changeToInitial);
+    document.getElementById("dt-change-lang-button").innerHTML="Eng";
+    lang="Eng";
+  }
+  console.log("changeLang\tDone");
+}
+
+function changeToNone(element) {
+  element.style.display="none";
+}
+
+function changeToInitial(element) {
+  element.style.display="initial";
+}
